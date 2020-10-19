ef6e6399e62f9126a7e54a6d2efead7f071d2bd4
Martyna Machowiak <martyna.machowiak@gmail.com>
2020-10-19 23:21:07 +0100

    Add <Header />

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>React Intro</h1>
+      <div>by STUDENT'S NAME</div>
+    </header>
+  );
+}
+
+export default Header;
