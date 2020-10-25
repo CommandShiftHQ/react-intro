c266b3ca527d7a2d9f580d3138f7af1f3077d0b4
Martyna Machowiak <martyna.machowiak@gmail.com>
2020-10-25 23:11:34 +0000

    Add <Dropdown />

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Dropdown from './components/Dropdown';
 import Header from './components/Header';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header name={author} />
+      <Dropdown />
     </div>
   );
 }
