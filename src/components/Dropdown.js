1eecad532175e4d301e1b43461817360c84ea805
Martyna Machowiak <martyna.machowiak@gmail.com>
2020-10-26 00:16:23 +0000

    Render .dropdown__body conditionally

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 const Dropdown = ({title, options}) => {
+  const isOpen = false;
 
   return (
     <div className="dropdown">
       <button className="dropdown__header">
         {title}
       </button>
-      <div className="dropdown__body">
-        <ul>
-          {options.map((option, i) => <li key={i} >{option}</li>)}
-        </ul>
-      </div>
+      {
+        isOpen && (
+          <div className="dropdown__body">
+            <ul>
+              {options.map((option, i) => <li key={i} >{option}</li>)}
+            </ul>
+          </div>
+        )
+      }
+
     </div>
   );
 }
